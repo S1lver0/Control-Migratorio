5725bafe6e541167cb24219f1cb7aeb828b98db8
Yahir Choquehuanca <yahirchoquehuanca@gmail.com>
2023-05-27 15:57:24 -0500

    se agrego validaciones

--- public/js/passTable.js
+++ public/js/passTable.js
@@ -1,4 +1,10 @@
+//objetos
 var pasajeros = [];
+var car;
+var ini;
+var driver;
+////
+var conteo=0;
 
 function agregarPasajero() {
   var nombre_apellidos = document.getElementById("id_Nombre_P").value;
@@ -97,6 +103,67 @@ function agregarPasajero() {
   });
   limpiarCampos();
 
+  ///convertir los select a numeros
+  //--para nacionalidad
+  var select_nacionalidad;
+  switch (campos[1]) {
+    case "Perú":
+      select_nacionalidad = 1;
+      break;
+    case "Chile":
+      select_nacionalidad = 2;
+      break;
+    case "Venezuela":
+      select_nacionalidad = 3;
+      break;
+    case "Brasil":
+      select_nacionalidad = 4;
+      break;
+    case "Ecuador":
+      select_nacionalidad = 5;
+      break;
+  }
+
+  //--para profesion
+  var select_profesion;
+  switch (campos[3]) {
+    case "Actor/actriz":
+      select_profesion = 1;
+      break;
+    case "Administrador/a de empresas":
+      select_profesion = 2;
+      break;
+    case "Agricultor/a":
+      select_profesion = 3;
+      break;
+    case "Arquitecto/a":
+      select_profesion = 4;
+      break;
+  }
+
+  //--para estado Civil
+  var select_estado;
+  switch (campos[2]) {
+    case "Soltero":
+      select_estado = 1;
+      break;
+    case "Casado":
+      select_estado = 2;
+      break;
+    case "Conviviente":
+      select_estado = 3;
+      break;
+    case "Viudo":
+      select_estado = 4;
+      break;
+    case "Divorciado":
+      select_estado = 4;
+      break;
+    case "Separado":
+      select_estado = 4;
+      break;
+  }
+
   //--
   pass = {
     nombre: campos[0],
@@ -105,9 +172,13 @@ function agregarPasajero() {
     profesion: campos[3],
     di: campos[5],
     fecha: campos[6],
+    select_estado: select_estado,
+    select_nacionalidad: select_nacionalidad,
+    select_profesion:select_profesion
   };
   pasajeros.push(pass);
   addTable();
+  conteo++;
 }
 
 function addTable() {
@@ -143,7 +214,7 @@ function deleteRow(button) {
     }
   }
   row.remove();
-  //mostrando 
+  //mostrando
   Swal.fire({
     icon: "success",
     title: "Eliminar",
@@ -179,3 +250,193 @@ function mensaje_Adv(mensaje_dv) {
     text: mensaje_dv,
   });
 }
+
+function valiAll() {
+  //VALIDAR CABECERA
+  var cabecera = document.getElementById("cabe");
+
+  if (
+    cabecera.querySelector("#Destino").value == "" ||
+    cabecera.querySelector("#Procedencia").value == ""
+  ) {
+    mensaje_Adv(
+      "Por favor, complete todos los campos requeridos antes de generar ficha"
+    );
+    return;
+  }
+  //Validar Vehiculo
+  var Vehiculo = document.getElementById("vehi1");
+  if (
+    Vehiculo.querySelector("#Tipo").value == "" ||
+    Vehiculo.querySelector("#Pais_Vehiculo").textContent ==
+      "Seleccione un pais" ||
+    Vehiculo.querySelector("#Marca").value == "" ||
+    Vehiculo.querySelector("#Modelo").value == "" ||
+    Vehiculo.querySelector("#Año").value == "" ||
+    Vehiculo.querySelector("#Motor").value == "" ||
+    Vehiculo.querySelector("#Chasis").value == "" ||
+    Vehiculo.querySelector("#Placa").value == ""
+  ) {
+    mensaje_Adv(
+      "Por favor, complete todos los campos requeridos antes de generar ficha"
+    );
+    return;
+  }
+  //Validar Conductor
+  var Conductor = document.getElementById("driver");
+  console.log(Conductor.querySelector("#Fecha_Nacimiento").value);
+  if (
+    Conductor.querySelector("#Pais_Conductor").textContent ==
+      "Seleccione un pais" ||
+    Conductor.querySelector("#MombreC").value == "" ||
+    Conductor.querySelector("#Domicilio").value == "" ||
+    Conductor.querySelector("#Profesion1").value == "" ||
+    Conductor.querySelector("#Brevete").value == "" ||
+    Conductor.querySelector("#Fecha_Nacimiento").value == "" ||
+    Conductor.querySelector("#sc_pp").value == ""
+  ) {
+    mensaje_Adv(
+      "Por favor, complete todos los campos requeridos antes de generar ficha"
+    );
+    return;
+  }
+  if(conteo==0){
+    mensaje_Adv(
+      "Tiene que ingresar al menos un pasajero"
+    );
+  }else{
+    addObj();
+  }
+}
+
+function addObj() {
+  //cabecera
+  var cabecera = document.getElementById("cabe");
+  var poliza = "no tiene";
+  if (cabecera.querySelector("#poliza").value != "") {
+    var poliza = cabecera.querySelector("#poliza").value;
+  }
+  ini = {
+    fecha: cabecera.querySelector("#fechaActual").value,
+    destino: cabecera.querySelector("#Destino").value,
+    procedencia: cabecera.querySelector("#Procedencia").value,
+    poliza: poliza,
+  };
+  //vehiculo
+  var Vehiculo = document.getElementById("vehi1");
+  var Vehi_tipo;
+  console.log(Vehiculo.querySelector("#Tipo").value);
+  switch (Vehiculo.querySelector("#Tipo").value) {
+    case "auto":
+      Vehi_tipo = 1;
+      break;
+    case "autobus":
+      Vehi_tipo = 2;
+      break;
+    case "moto":
+      Vehi_tipo = 3;
+      break;
+    case "camioneta":
+      Vehi_tipo = 4;
+      break;
+    case "camion":
+      Vehi_tipo = 5;
+      break;
+  }
+
+  var Vehi_Pais;
+
+  switch (Vehiculo.querySelector("#Pais_Vehiculo").textContent) {
+    case "Perú":
+      Vehi_Pais = 1;
+      break;
+    case "Chile":
+      Vehi_Pais = 2;
+      break;
+    case "Venezuela":
+      Vehi_Pais = 3;
+      break;
+    case "Brasil":
+      Vehi_Pais = 4;
+      break;
+    case "Ecuador":
+      Vehi_Pais = 5;
+      break;
+  }
+
+  car = {
+    tipo: Vehi_tipo,
+    marca: Vehiculo.querySelector("#Marca").value,
+    modelo: Vehiculo.querySelector("#Modelo").value,
+    año: Vehiculo.querySelector("#Año").value,
+    motor: Vehiculo.querySelector("#Motor").value,
+    chasis: Vehiculo.querySelector("#Chasis").value,
+    placa: Vehiculo.querySelector("#Placa").value,
+    pais: Vehi_Pais,
+  };
+
+  //Conductor
+  var Conductor = document.getElementById("driver");
+  var conductor_Profesion;
+  switch (Conductor.querySelector("#Profesion1").value) {
+    case "Actor/actriz":
+      conductor_Profesion = 1;
+      break;
+    case "Administrador/a de empresas":
+      conductor_Profesion = 2;
+      break;
+    case "Agricultor/a":
+      conductor_Profesion = 3;
+      break;
+    case "Arquitecto/a":
+      conductor_Profesion = 4;
+      break;
+  }
+
+  var conductor_Pais;
+  switch (Conductor.querySelector("#Pais_Conductor").textContent) {
+    case "Perú":
+      conductor_Pais = 1;
+      break;
+    case "Chile":
+      conductor_Pais = 2;
+      break;
+    case "Venezuela":
+      conductor_Pais = 3;
+      break;
+    case "Brasil":
+      conductor_Pais = 4;
+      break;
+    case "Ecuador":
+      conductor_Pais = 5;
+      break;
+  }
+
+  driver = {
+    nombre: Conductor.querySelector("#MombreC").value,
+    domicilio: Conductor.querySelector("#Domicilio").value,
+    profesion: conductor_Profesion,
+    nacionalidad: conductor_Pais,
+    brevete: Conductor.querySelector("#Brevete").value,
+    fecha_na: Conductor.querySelector("#Fecha_Nacimiento").value,
+    scpp: Conductor.querySelector("#sc_pp").value,
+  };
+
+  console.log(ini);
+  console.log(car);
+  console.log(driver);
+  GenerarJson();
+}
+
+function GenerarJson(){
+  var ObjAll = {
+    pasajeros:pasajeros,
+    car:car,
+    driver:driver,
+    ini:ini
+  }
+  console.log(ObjAll);
+}
+
+
+
